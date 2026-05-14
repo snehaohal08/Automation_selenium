54e79e4af38cfc40516ec8cfce6fd4ab823a6940
sneha <snehaohal08@gmail.com>
2026-05-14 10:03:33 +0530

    drag and drop

---
+++ src/main/java/Test/Selenium_automation/ExplicitWait_Ex.java
@@ -0,0 +1,11 @@
+package Test.Selenium_automation;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExplicitWait_Ex {
+	public static void main(String args[]) {
+		ChromeDriver cd = new ChromeDriver();
+		cd.get("https://demo.automationtesting.in/Register.html");
+	}
+
+}
